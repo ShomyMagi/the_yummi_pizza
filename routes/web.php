98a7152edab7dfee95f3ed15c6d940e233e91825
Milos Simic <43641238+ShomyMagi@users.noreply.github.com>
2020-07-19 20:42:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'ProductController@index');
-Route::get('/products', 'ProductController@getProducts');
-Route::get('/cart', 'CartController@index');
-Route::get('/cart/getCart', 'CartController@getCart');
-Route::post('/addToCart/{id}', 'CartController@addToCart');
-Route::get('/deleteCart/{id}', 'CartController@deleteFromCart');
-Route::get('/cart/countCart', 'CartController@countCart');
-Route::post('/order', 'OrderController@order');
-Route::get('/cart/delete', 'CartController@deleteCart');
+Route::group(array('https'), function()
+{
+    Route::get('/', 'ProductController@index');
+    Route::get('/products', 'ProductController@getProducts');
+    Route::get('/cart', 'CartController@index');
+    Route::get('/cart/getCart', 'CartController@getCart');
+    Route::post('/addToCart/{id}', 'CartController@addToCart');
+    Route::get('/deleteCart/{id}', 'CartController@deleteFromCart');
+    Route::get('/cart/countCart', 'CartController@countCart');
+    Route::post('/order', 'OrderController@order');
+    Route::get('/cart/delete', 'CartController@deleteCart');
+});
